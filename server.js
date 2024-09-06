9ed0daf14b5b29e1b0eb5f8c9d9a9dffb6080f1d
Zodd <zodd@Zodd-PC.home>
2024-09-06 14:01:26 +0200

    Mount the users routes in server.js commit

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 
 const db = require('./db.js');
 const express = require('express');
+const userRoutes = require('./routes/user.js');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use('/users', userRoutes);
 
 db.sync({ force: false })
   .then(() => {
